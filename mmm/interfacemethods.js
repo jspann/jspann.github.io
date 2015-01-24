7d54e3d198200110d025483692e26f4dca50ff29
James Spann <thedeveloper4@gmail.com>
2015-01-24 18:43:23 -0500

    Connecting button to compute function

--- mmm/interfacemethods.js
+++ mmm/interfacemethods.js
@@ -20,15 +20,13 @@ function rgbToHex(r, g, b) {
 }
 
 function domReady() {
-
   var d = new Date();
   var n = d.getFullYear();
   var t = "© James Spann " + n;
   document.getElementById("jamescopyright").innerHTML = t;
   console.log(t);
   if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-   // some code..
-   alert("Warning!\nThis program might not function properly when viewed on a mobile device!");
+    alert("Warning!\nThis program might not function properly when viewed on a mobile device!");
   }
 
   var canvas = document.getElementById("myCanvas");
@@ -37,9 +35,14 @@ function domReady() {
   ctx.fillRect(0,0,document.getElementById("myCanvas").width,document.getElementById("myCanvas").height);
 
   var grd = ctx.createLinearGradient(0, 200, 0, 0);
-grd.addColorStop(0, "red");
-grd.addColorStop(1, "purple");
+  grd.addColorStop(0, "red");
+  grd.addColorStop(1, "purple");
 
-ctx.fillStyle = grd;
-ctx.fillRect(0,0,document.getElementById("myCanvas").width,document.getElementById("myCanvas").height);
+  ctx.fillStyle = grd;
+  ctx.fillRect(0,0,document.getElementById("myCanvas").width,document.getElementById("myCanvas").height);
 }
+
+document.getElementById("gobutton").onclick = function() {
+   alert("button was clicked");
+   compute(myidentifier);
+}​;​
