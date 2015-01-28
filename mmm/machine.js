6bc567f46245b5981a37a5be94e1f9addc6aa5c6
James Spann <thedeveloper4@gmail.com>
2015-01-28 18:41:12 -0500

    Writing new methods for getting friend data

--- mmm/machine.js
+++ mmm/machine.js
@@ -17,3 +17,49 @@ function train(){
 function runmethod(){
 	alert("nice bro. Lets do this");
 }
+
+function getMusic(myid){
+  FB.api("/me/music",function (response) {
+  if (response && !response.error) {
+  intermusarray = response.data;
+  for(var p = 0; p < intermusarray.length; p++){
+  console.log(intermusarray[p]);
+  }
+  }
+  });//end of music api call
+}
+
+function getTelevison(myid){
+  FB.api("/me/television",function (response) {
+    if (response && !response.error) {
+      intertvarray = response.data;
+      for(var p = 0; p < intertvarray.length; p++){
+      console.log(intertvarray[p]);
+      }
+    }
+  });//end of tv api call
+}
+
+function getBooks(myid){
+  FB.api("/me/books",function (response) {
+  if (response && !response.error) {
+  interbookarray = response.data;
+  for(var p = 0; p < interbookarray.length; p++){
+  console.log(interbookarray[p]);
+  }
+  }
+  });//end of book api call
+}
+
+function getMovies(myid){
+  FB.api("/me/movies",function (response) {
+  if (response && !response.error) {
+  interbookarray = response.data;
+  for(var p = 0; p < interbookarray.length; p++){
+  console.log(interbookarray[p]);
+  }
+  }
+  });//end of movies api call
+
+  //MAYBE we'll do likes in the future
+}
